07e3526fde2efcfd5de478282f365faa182ffbce
JiyunHwang1008 <kuss7732@sookmyung.ac.kr>
2025-10-02 15:36:17 +0900

    05

--- main.c
+++ main.c
@@ -4,21 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) 
-	{
+{
 	int x;
+	int i;
+	int sum=0;
 	
 	printf("Input an integer :");
 	scanf("%d", &x);
 	
-	if (x>0){
-		printf("absolute is %i.\n",x);
-	}
+	for(i=1; i<=x  ;i++)
+	   sum=sum+i;
+	   
+	printf("sum is %i\n", sum);   
 	
-      
-    else{
-    	printf("absolute is %i.\n",-x);
-	}
-     
+
       
       system("PAUSE");
 	   
