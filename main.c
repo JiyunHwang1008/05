f5a654f7f69d44feb2f0e5691dd0a0f4b2c83fe3
JiyunHwang1008 <kuss7732@sookmyung.ac.kr>
2025-10-02 15:31:06 +0900

    05

--- main.c
+++ main.c
@@ -3,18 +3,22 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+	{
 	int x;
 	
 	printf("Input an integer :");
 	scanf("%d", &x);
 	
-	if (x>0)
-	   printf("양수입니다.\n");
-	else if(x<0)
-	     printf("음수입니다.\n");
-    else
-      printf("0입니다.\n");
+	if (x>0){
+		printf("absolute is %i.\n",x);
+	}
+	
+      
+    else{
+    	printf("absolute is %i.\n",-x);
+	}
+     
       
       system("PAUSE");
 	   
